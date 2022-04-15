4a9b902941cd262965dcec6bcebf5fc930556e22
Matt Marlow <thequantumblink@gmail.com>
2022-04-15 14:07:34 -0500

    Initial Commit

---
+++ src/main/java/com/idtech/block/BlockMod.java
@@ -0,0 +1,52 @@
+package com.idtech.block;
+
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+//import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.Material;
+//import net.minecraftforge.common.ToolType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class BlockMod {
+
+    //Basic Block
+    public static final Block CASTLE_WALL = BlockUtils.createBasicBlock("castlewall", Material.STONE);
+    public static final Item CASTLE_WALL_ITEM = BlockUtils.createBlockItem(CASTLE_WALL, CreativeModeTab.TAB_MISC);
+    public static final Block GEL_ORE = BlockUtils.createBasicBlock("geloreblock", Material.STONE);
+    public static final Item GEL_ORE_ITEM = BlockUtils.createBlockItem(GEL_ORE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+
+    @SubscribeEvent
+    public static void registerBlockItems(RegistryEvent.Register<Item> event) {
+
+        event.getRegistry().register(CASTLE_WALL_ITEM);
+        event.getRegistry().register(GEL_ORE_ITEM);
+        event.getRegistry().register(RubberBlock.ITEM);
+        event.getRegistry().register(CreepingMoldBlock.ITEM);
+        event.getRegistry().register(CreeperSurpriseBlock.ITEM);
+
+
+
+    }
+
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event) {
+
+        event.getRegistry().register(CASTLE_WALL);
+        event.getRegistry().register(GEL_ORE);
+        event.getRegistry().register(RubberBlock.Instance);
+        event.getRegistry().register(CreepingMoldBlock.Instance);
+        event.getRegistry().register(CreeperSurpriseBlock.Instance);
+
+
+    }
+}
+
+
+
+
+
---
+++ src/main/java/com/idtech/item/ArmorStatistics.java
@@ -0,0 +1,79 @@
+package com.idtech.item;
+
+import com.idtech.BaseMod;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
+
+public enum ArmorStatistics implements ArmorMaterial {
+    GEL("gel", 10, new int[]{3, 5, 4, 2}, 12,SoundEvents.ARMOR_EQUIP_CHAIN, 1.5f, 0.0f, () -> Ingredient.of(ItemMod.STRUCTURE_GEL));
+    ;
+
+
+    private static final int[] HEALTH_FOR_SLOT = new int[]{3, 6, 9, 12};
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] slotProtections;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
+    ArmorStatistics(String name, int durabilityMultiplier, int[] slotProtections, int enchantmentValue, SoundEvent sound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.slotProtections = slotProtections;
+        this.enchantmentValue = enchantmentValue;
+        this.sound = sound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = new LazyLoadedValue<>(repairIngredient);
+    }
+
+    @Override
+    public int getDurabilityForSlot(EquipmentSlot p_40410_) {
+        return HEALTH_FOR_SLOT[p_40410_.getIndex()]*this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getDefenseForSlot(EquipmentSlot p_40411_) {
+        return this.slotProtections[p_40411_.getIndex()];
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    @Override
+    public @NotNull SoundEvent getEquipSound() {
+        return this.sound;
+    }
+
+    @Override
+    public @NotNull Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return BaseMod.MODID+":"+this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
