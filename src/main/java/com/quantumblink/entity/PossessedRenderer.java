62a640c808ad95a11e06dcded5e8a773c135ab9e
Matt Marlow <thequantumblink@gmail.com>
2022-06-25 22:43:31 -0500

    rendering functional for mob

--- src/main/java/com/quantumblink/entity/PossessedRenderer.java
+++ src/main/java/com/quantumblink/entity/PossessedRenderer.java
@@ -1,16 +1,21 @@
 package com.quantumblink.entity;
 
 import com.quantumblink.BaseMod;
+import net.minecraft.client.model.ZombieModel;
+import net.minecraft.client.renderer.entity.AbstractZombieRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.ZombieRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.monster.Zombie;
 import org.jetbrains.annotations.NotNull;
 
 public class PossessedRenderer extends ZombieRenderer {
-    public PossessedRenderer(EntityRendererProvider.Context provider) {
-        super(provider);
+    private static final ResourceLocation TEXTURE = new ResourceLocation(BaseMod.MODID, "textures/entity/possessed.png");
+    public PossessedRenderer(EntityRendererProvider.Context p_174456_) {
+        super(p_174456_);
     }
-    public ResourceLocation getTextureLocation(@NotNull PossessedEntity entity) {
-        return new ResourceLocation(BaseMod.MODID, "textures/entity/possessed.png");
+    @Override
+    public @NotNull ResourceLocation getTextureLocation(@NotNull Zombie p_113771_) {
+        return TEXTURE;
     }
 }
