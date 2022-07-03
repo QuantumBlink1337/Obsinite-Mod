54e56690a7199b552ffce2b98a766f0a258e48bf
Matt Marlow <thequantumblink@gmail.com>
2022-07-03 14:06:25 -0500

    manually registering features

--- src/main/java/com/quantumblink/worldgen/Ores.java
+++ src/main/java/com/quantumblink/worldgen/Ores.java
@@ -1,39 +1,39 @@
 package com.quantumblink.worldgen;
 
+import com.quantumblink.BaseMod;
 import com.quantumblink.block.BlockMod;
 import net.minecraft.core.Holder;
-import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.core.Registry;
+import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.placement.*;
 import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
-
+import java.util.List;
 
 public class Ores {
 
-    public static final int OVERWORLD_VEINSIZE = 5;
-    public static final int OVERWORLD_AMOUNT = 3;
+    public static final int OVERWORLD_VEINSIZE = 10;
+    public static final int OVERWORLD_AMOUNT = 50;
 
-    public static Holder<PlacedFeature> OVERWORLD_OREGEN;
+    public static PlacedFeature OVERWORLD_OREGEN_PF;
+    public static ConfiguredFeature<OreConfiguration, Feature<OreConfiguration>> OVERWORLD_OREGEN_CF;
     public static void registerConfiguredFeatures() {
         OreConfiguration overworldConfig = new OreConfiguration(new BlockMatchTest(Blocks.CALCITE), BlockMod.CINNABAR_ORE.get().defaultBlockState(),OVERWORLD_VEINSIZE);
-        OVERWORLD_OREGEN = registerPlacedFeature("cinnabar_ore_overworld", new ConfiguredFeature<>(Feature.ORE, overworldConfig),
-                CountPlacement.of(OVERWORLD_AMOUNT),
-                InSquarePlacement.spread(),
-                HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(60)));
-    }
-
-    private static <C extends FeatureConfiguration, F extends Feature<C>> Holder<PlacedFeature> registerPlacedFeature(String registryName, ConfiguredFeature<C, F> feature, PlacementModifier... placementModifiers) {
-        return PlacementUtils.register(registryName, Holder.direct(feature), placementModifiers);
+        OVERWORLD_OREGEN_CF = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new ResourceLocation(BaseMod.MODID, "cinnabar_ore_overworld"), new ConfiguredFeature<>(Feature.ORE, overworldConfig));
+        OVERWORLD_OREGEN_PF =  Registry.register(BuiltinRegistries.PLACED_FEATURE, new ResourceLocation(BaseMod.MODID, "cinnabar_ore_overworld"), new PlacedFeature(Holder.direct(OVERWORLD_OREGEN_CF),
+                List.of(CountPlacement.of(OVERWORLD_AMOUNT),
+                        InSquarePlacement.spread(),
+                        HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(60)))));
     }
 
     public static void onBiomeLoadingEvent(BiomeLoadingEvent event) {
-            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, OVERWORLD_OREGEN);
+            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Holder.direct(OVERWORLD_OREGEN_PF));
     }
 }
