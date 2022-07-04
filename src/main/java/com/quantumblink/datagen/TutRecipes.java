5af9647adcf4ac1309c703678565044d023604f6
Matt Marlow <thequantumblink@gmail.com>
2022-07-03 20:50:34 -0500

    new recipes for processing cinnabar into mercury

--- src/main/java/com/quantumblink/datagen/TutRecipes.java
+++ src/main/java/com/quantumblink/datagen/TutRecipes.java
@@ -1,21 +1,32 @@
 package com.quantumblink.datagen;
 
+import com.quantumblink.block.BlockMod;
 import com.quantumblink.item.ItemMod;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.SmeltingRecipe;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
-
+// https://docs.minecraftforge.net/en/1.18.x/datagen/server/recipes/
 public class TutRecipes extends RecipeProvider {
 
     public TutRecipes(DataGenerator generatorIn) {
         super(generatorIn);
     }
-
     @Override
-    protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
+    protected void buildCraftingRecipes(@NotNull Consumer<FinishedRecipe> consumer) {
 
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(ItemMod.CINNABAR_DUST.get()), ItemMod.MERCURY.get(), 2f, 400)
+                .unlockedBy("has_cinnabar_dust",has(ItemMod.CINNABAR_DUST.get()))
+                .save(consumer, "cinnabar_dust_to_mercury");
+        SingleItemRecipeBuilder.stonecutting(Ingredient.of(ItemMod.CINNABAR.get()), ItemMod.CINNABAR_DUST.get())
+                .unlockedBy("has_cinnabar", has(ItemMod.CINNABAR.get()))
+                .save(consumer, "cinnabar_to_cinnabar_dust");
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(BlockMod.CINNABAR_ORE_ITEM.get()), ItemMod.CINNABAR.get(), 2f, 200)
+                .unlockedBy("has_cinnabar_ore", has(BlockMod.CINNABAR_ORE_ITEM.get()))
+                .save(consumer, "cinnabar_ore_to_crystals");
     }
 }
