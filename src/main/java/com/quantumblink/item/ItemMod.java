ec2434c13804c0018161bdb751d43a5f48f2904b
Matt Marlow <thequantumblink@gmail.com>
2022-07-05 21:34:51 -0500

    modified some stats for the Obsinite tools

--- src/main/java/com/quantumblink/item/ItemMod.java
+++ src/main/java/com/quantumblink/item/ItemMod.java
@@ -33,7 +33,7 @@ public class ItemMod {
     public static final RegistryObject<Item> CRYSTALLINE_MIX = ITEMS.register("crystalline_mix", () -> new Item(ITEM_PROPERTIES));
 
 
-    public static final Tier TIER = new ForgeTier(5, 2350, 10F, 4.2F, 16,
+    public static final Tier TIER = new ForgeTier(5, 2350, 11.2F, 4.2F, 16,
             BlockTags.create(new ResourceLocation("needs_obsinite_tool")), ()-> Ingredient.of(OBSINITE.get()));
 
     //FOODS
@@ -45,11 +45,10 @@ public class ItemMod {
     public static final RegistryObject<Item> OBSINITE_LEGGINGS = TOOLS.register("obsinite_leggings", () -> new ArmorItem(ArmorStatistics.OBSINITE, EquipmentSlot.LEGS, ITEM_PROPERTIES.fireResistant()));
     public static final RegistryObject<Item> OBSINITE_BOOTS = TOOLS.register("obsinite_boots", () -> new ArmorItem(ArmorStatistics.OBSINITE, EquipmentSlot.FEET, ITEM_PROPERTIES.fireResistant()));
 
-    public static final RegistryObject<Item> OBSINITE_SWORD = TOOLS.register("obsinite_sword", () -> new SwordItem(TIER, 1, 1.0f, ITEM_PROPERTIES.fireResistant()));
-    public static final RegistryObject<Item> OBSINITE_PICKAXE = TOOLS.register("obsinite_pickaxe", () -> new PickaxeItem(TIER, 1, 1.0f, ITEM_PROPERTIES.fireResistant()));
-    public static final RegistryObject<Item> OBSINITE_SHOVEL = TOOLS.register("obsinite_shovel", () -> new ShovelItem(TIER, 1, 1.0f, ITEM_PROPERTIES.fireResistant()));
-    public static final RegistryObject<Item> OBSINITE_AXE = TOOLS.register("obsinite_axe", () -> new AxeItem(TIER, 1, 1.0f, ITEM_PROPERTIES.fireResistant()));
-    public static final RegistryObject<Item> OBSINITE_HOE = TOOLS.register("obsinite_hoe", () -> new HoeItem(TIER, 1, 1.0f, ITEM_PROPERTIES.fireResistant()));
+    public static final RegistryObject<Item> OBSINITE_SWORD = TOOLS.register("obsinite_sword", () -> new SwordItem(TIER, 4, -1.5f, ITEM_PROPERTIES.fireResistant()));
+    public static final RegistryObject<Item> OBSINITE_PICKAXE = TOOLS.register("obsinite_pickaxe", () -> new PickaxeItem(TIER, (int) 1.5F, -3.0f, ITEM_PROPERTIES.fireResistant()));
+    public static final RegistryObject<Item> OBSINITE_SHOVEL = TOOLS.register("obsinite_shovel", () -> new ShovelItem(TIER, 1, -2.8f, ITEM_PROPERTIES.fireResistant()));
+    public static final RegistryObject<Item> OBSINITE_AXE = TOOLS.register("obsinite_axe", () -> new AxeItem(TIER, 5, -3.0f, ITEM_PROPERTIES.fireResistant()));
+    public static final RegistryObject<Item> OBSINITE_HOE = TOOLS.register("obsinite_hoe", () -> new HoeItem(TIER, -4, 0f, ITEM_PROPERTIES.fireResistant()));
 
-    //public static final ArmorItem GEL_HELMET = (ArmorItem) new ArmorItem(ArmorStatistics.GEL, EquipmentSlot.HEAD, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)).setRegistryName(BaseMod.MODID, "gel_helmet");
 }
