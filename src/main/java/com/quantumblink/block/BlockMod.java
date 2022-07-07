79e19694214e948fdd924e11b9df31d8d19845e9
Matt Marlow <thequantumblink@gmail.com>
2022-07-07 07:58:18 -0500

    implemented Obsinite Block

--- src/main/java/com/quantumblink/block/BlockMod.java
+++ src/main/java/com/quantumblink/block/BlockMod.java
@@ -5,6 +5,7 @@ import com.quantumblink.item.ItemMod;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,6 +20,10 @@ public class BlockMod {
     public static final RegistryObject<Item> CINNABAR_ORE_ITEM = blockToBlockItem(CINNABAR_ORE);
     public static final RegistryObject<Block> MERCURIZED_OBSIDIAN = BLOCKS.register("mercurized_obsidian", () -> MercurizedObsidianBlock.INSTANCE);
     public static final RegistryObject<Item> MERCURIZED_OBSIDIAN_ITEM = blockToBlockItem(MERCURIZED_OBSIDIAN);
+    public static final RegistryObject<Block> OBSINITE_BLOCK = BLOCKS.register("obsinite_block", () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL)
+            .requiresCorrectToolForDrops().strength(3.5f)));
+    public static final RegistryObject<Item> OBSINITE_BLOCK_ITEM = blockToBlockItem(OBSINITE_BLOCK);
+
 
     public static <B extends Block>RegistryObject<Item> blockToBlockItem(RegistryObject<B> block) {
         return ItemMod.ITEMS.register(block.getId().getPath(), () -> new BlockItem(block.get(), ItemMod.ITEM_PROPERTIES));
