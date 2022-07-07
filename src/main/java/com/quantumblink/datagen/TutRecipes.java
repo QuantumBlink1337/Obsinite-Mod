1cb22957195b72260f7d04f98d7f5f6460e09f0b
Matt Marlow <thequantumblink@gmail.com>
2022-07-07 07:58:37 -0500

    models for obsinite block

--- src/main/java/com/quantumblink/datagen/TutRecipes.java
+++ src/main/java/com/quantumblink/datagen/TutRecipes.java
@@ -41,16 +41,28 @@ public class TutRecipes extends RecipeProvider {
             dyes.add(Items.YELLOW_DYE);
             dyes.add(Items.LIGHT_BLUE_DYE);
 
-        List<ArmorItem> NETHERITE_ARMOR = new ArrayList<>();
-            NETHERITE_ARMOR.add((ArmorItem) Items.NETHERITE_HELMET);
-            NETHERITE_ARMOR.add((ArmorItem) Items.NETHERITE_CHESTPLATE);
-            NETHERITE_ARMOR.add((ArmorItem) Items.NETHERITE_LEGGINGS);
-            NETHERITE_ARMOR.add((ArmorItem) Items.NETHERITE_BOOTS);
-        List<ArmorItem> OBSINITE_ARMOR = new ArrayList<>();
-            OBSINITE_ARMOR.add((ArmorItem) ItemMod.OBSINITE_HELMET.get());
-            OBSINITE_ARMOR.add((ArmorItem) ItemMod.OBSINITE_CHEST.get());
-            OBSINITE_ARMOR.add((ArmorItem) ItemMod.OBSINITE_LEGGINGS.get());
-            OBSINITE_ARMOR.add((ArmorItem) ItemMod.OBSINITE_BOOTS.get());
+        List<Item> NETHERITE_ARMOR = new ArrayList<>();
+            NETHERITE_ARMOR.add( Items.NETHERITE_HELMET);
+            NETHERITE_ARMOR.add( Items.NETHERITE_CHESTPLATE);
+            NETHERITE_ARMOR.add( Items.NETHERITE_LEGGINGS);
+            NETHERITE_ARMOR.add( Items.NETHERITE_BOOTS);
+        List<Item> OBSINITE_ARMOR = new ArrayList<>();
+            OBSINITE_ARMOR.add( ItemMod.OBSINITE_HELMET.get());
+            OBSINITE_ARMOR.add( ItemMod.OBSINITE_CHEST.get());
+            OBSINITE_ARMOR.add( ItemMod.OBSINITE_LEGGINGS.get());
+            OBSINITE_ARMOR.add( ItemMod.OBSINITE_BOOTS.get());
+        List<Item> NETHERITE_TOOLS = new ArrayList<>();
+        NETHERITE_TOOLS.add( Items.NETHERITE_SWORD);
+        NETHERITE_TOOLS.add( Items.NETHERITE_PICKAXE);
+        NETHERITE_TOOLS.add( Items.NETHERITE_SHOVEL);
+        NETHERITE_TOOLS.add( Items.NETHERITE_AXE);
+        NETHERITE_TOOLS.add( Items.NETHERITE_HOE);
+        List<Item> OBSINITE_TOOLS = new ArrayList<>();
+        OBSINITE_TOOLS.add( ItemMod.OBSINITE_SWORD.get());
+        OBSINITE_TOOLS.add( ItemMod.OBSINITE_PICKAXE.get());
+        OBSINITE_TOOLS.add( ItemMod.OBSINITE_SHOVEL.get());
+        OBSINITE_TOOLS.add( ItemMod.OBSINITE_AXE.get());
+        OBSINITE_TOOLS.add( ItemMod.OBSINITE_HOE.get());
         SimpleCookingRecipeBuilder.blasting(Ingredient.of(ItemMod.CINNABAR_DUST.get()), ItemMod.MERCURY.get(), 2f, 400)
                 .unlockedBy("has_cinnabar_dust",has(ItemMod.CINNABAR_DUST.get()))
                 .save(consumer, recipeName("cinnabar_dust_to_mercury"));
@@ -74,6 +86,12 @@ public class TutRecipes extends RecipeProvider {
                     .save(consumer, recipeName(OBSINITE_ARMOR.get(i).toString()));
 
         }
+        for (int i = 0; i < NETHERITE_TOOLS.size(); i++) {
+            UpgradeRecipeBuilder.smithing(Ingredient.of(NETHERITE_TOOLS.get(i)), Ingredient.of(ItemMod.OBSINITE.get()), OBSINITE_TOOLS.get(i))
+                    .unlocks("has_obsinite_ingot", has(ItemMod.OBSINITE.get()))
+                    .save(consumer, recipeName(OBSINITE_TOOLS.get(i).toString()));
+
+        }
         ShapelessRecipeBuilder.shapeless(ItemMod.CRYSTALLINE_MIX.get())
                 .requires(ItemMod.CINNABAR.get())
                 .requires(Items.AMETHYST_SHARD)
@@ -81,6 +99,18 @@ public class TutRecipes extends RecipeProvider {
                 .requires(Items.LAPIS_LAZULI)
                 .unlockedBy("has_cinnabar", has(ItemMod.CINNABAR.get()))
                 .save(consumer, recipeName("crystalline_mix_recipe"));
+        ShapedRecipeBuilder.shaped(BlockMod.OBSINITE_BLOCK_ITEM.get())
+                .pattern("aaa")
+                .pattern("aaa")
+                .pattern("aaa")
+                .define('a', ItemMod.OBSINITE.get())
+                .unlockedBy("has_obsinite_ingot",has(ItemMod.OBSINITE.get()))
+                .save(consumer, recipeName("obsinite_ingot_to_block"));
+        ShapelessRecipeBuilder.shapeless(ItemMod.OBSINITE.get(), 9)
+                .requires(BlockMod.OBSINITE_BLOCK_ITEM.get())
+                .unlockedBy("has_obsinite_ingot",has(ItemMod.OBSINITE.get()))
+                .save(consumer, recipeName("obsinite_block_to_ingot"));
+
     }
     private String recipeName(String string) {
         return BaseMod.MODID + ":" + string;
